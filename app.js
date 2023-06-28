28db2c1cfdb6dff9f230b50d74bad2f5e9adcf80
KhaledGharib <90965285+KhaledGharib@users.noreply.github.com>
2023-06-28 07:50:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
+// CORS configuration
 app.use((req, res, next) => {
-  const allowedOrigins = ["https://statuesque-cuchufli-c71e7e.netlify.app","http://localhost:3000"]; // Add any other origins you want to allow
+  const allowedOrigins = [
+    "https://statuesque-cuchufli-c71e7e.netlify.app",
+    "http://localhost:3000",
+  ]; // Add any other origins you want to allow
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
@@ -17,33 +21,31 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/proxy/:query", (req, res) => {
-  const query = req.params.query;
-  const url = `https://3sktv.org/?s=${query}`;
+app.get("/proxy/:query", async (req, res) => {
+  try {
+    const query = req.params.query;
+    const url = `https://3sktv.org/?s=${query}`;
 
-  axios
-    .get(url)
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      const links = [];
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const links = [];
 
-      $(".type_item_box").each(function () {
-        const link = $(this).find("a").attr("href");
-        const title = $(this).find("a").attr("title");
-        const img = $(this).find(".item_img.imaged").attr("src");
-        links.push({
-          title,
-          link,
-          img,
-        });
+    $(".type_item_box").each(function () {
+      const link = $(this).find("a").attr("href");
+      const title = $(this).find("a").attr("title");
+      const img = $(this).find(".item_img.imaged").attr("src");
+      links.push({
+        title,
+        link,
+        img,
       });
-
-      res.json(links);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: "Error occurred while fetching data" });
     });
+
+    res.json(links);
+  } catch (error) {
+    res.status(500).json({ error: "Error occurred while fetching data" });
+  }
 });
 
 app.listen(port, () => {
